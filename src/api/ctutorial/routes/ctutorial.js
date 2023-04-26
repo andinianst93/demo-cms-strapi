87e69b734771b216024f2b1369bc00aedfb10675
andinianst93 <andinianst93@gmail.com>
2023-04-26 15:36:22 +0700

    ok

---
+++ src/api/ctutorial/routes/ctutorial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ctutorial router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ctutorial.ctutorial');
---
+++ src/api/ctutorial/services/ctutorial.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ctutorial service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ctutorial.ctutorial');
